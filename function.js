0fd9531d804c25e46315c87cb8f540f4a1444a46
niveditalodha <niveditalodha1234@gmail.com>
2021-04-02 16:16:07 +0530

    Final

--- function.js
+++ function.js
@@ -1,3 +1,29 @@
+function init(){
+  $link ="https://corona-api.com/timeline";
+  $.getJSON($link, function(data) {
+    document.getElementById('newConfirmed').innerHTML = "<i class='fa fa-arrow-up' aria-hidden='true'></i> "+data['data'][0]['new_confirmed'].toLocaleString();
+    document.getElementById('confirmed').innerHTML = data['data'][0]['confirmed'].toLocaleString();
+    document.getElementById('active').innerHTML = data['data'][0]['active'].toLocaleString();
+    document.getElementById('newRecovered').innerHTML = "<i class='fa fa-arrow-up' aria-hidden='true'></i> "+data['data'][0]['new_recovered'].toLocaleString();
+    document.getElementById('recovered').innerHTML = data['data'][0]['recovered'].toLocaleString();
+    document.getElementById('newDeaths').innerHTML = "<i class='fa fa-arrow-up' aria-hidden='true'></i> "+data['data'][0]['new_deaths'].toLocaleString();
+    document.getElementById('deaths').innerHTML = data['data'][0]['deaths'].toLocaleString();
+    document.getElementById('ratios').innerHTML = 'Death Rate: '+(data["data"][0]["deaths"]*100/data['data'][0]['confirmed']).toFixed(2)+'\
+                                                    % <br/>\
+                                                    Recovery Rate:\
+                                                    '+(data["data"][0]["recovered"]*100/data["data"][0]["confirmed"]).toFixed(2)+'%';
+  });
+  $link = "https://corona-api.com/countries";
+  $.getJSON($link, function(data) {
+
+    for(var i =0; i<data['data'].length;i++){
+      $('select').append('<option value = "'+i+'">'+data['data'][i]['name']+'</option>');
+    }
+    $('.selectpicker').selectpicker('refresh');
+  });
+
+}
+
 $link ="https://corona-api.com/timeline";
 $.getJSON($link,function(data){
   var date = data['data'][0]['updated_at'];
